9c61abd321ba2ec74560578d4da14f7cc4175011
Juan Carlos Vallejo <jcvallejo@start-app.es>
2018-02-03 20:57:49 +0100

    Initial release

--- aca/src/release/java/com/urizev/aca/utils/RxUtils.java
+++ aca/src/release/java/com/urizev/aca/utils/RxUtils.java
@@ -3,28 +3,15 @@ package com.urizev.aca.utils;
 import okhttp3.Interceptor;
 
 public class RxUtils {
-    private RxUtils() {}
+    private RxUtils() { }
 
-    public static void assertMainThread() {
-    }
+    public static void assertMainThread() { }
 
-    public static void assertNonMainThread() {
-    }
+    public static void assertNonMainThread() { }
 
-    public static void assertIOThread() {
+    public static void assertIOThread() { }
 
-    }
+    public static void assertComputationThread() { }
 
-    public static void assertComputationThread() {
-
-    }
-
-    private static void assertThreadPrefix(String prefix) {
-    }
-
-    public static Interceptor interceptor() {
-        return chain -> {
-            return chain.proceed(chain.request());
-        };
-    }
+    public static Interceptor interceptor() { return chain -> chain.proceed(chain.request()); }
 }
